7babdb8d98354b99caeb835719c9f51152d4a408
Adam Kalt <adam.kalt.1@gmail.com>
2018-04-18 03:26:04 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -3,7 +3,7 @@ your_hash = {"key" => "value"}
 end
 
 def shipping_manifest
-  the_manifest = {"whale_bone_corsets" => 5, "porcelain_vases" => 2, "oil_paintings" => 3}
+  the_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
 end
 
 def retrieval
